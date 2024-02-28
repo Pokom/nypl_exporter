b050b4da0c4a318caf024a7aba2e71bea01e3a14
pokom <mark.poko@grafana.com>
2024-02-28 14:51:18 -0500

    Initialize branch

--- main.go
+++ main.go
@@ -3,38 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"nypl_exporter/pkg"
-)
-
-const (
-	defaultUrl = "https://api.repo.nypl.org/api/v2/"
 )
 
 func main() {
-	key := os.Getenv("NYPL_API_KEY")
-	if key == "" {
-		fmt.Println("NYPL_API_KEY must be set")
-		os.Exit(1)
-	}
-	url := os.Getenv("NYPL_API_URL")
-	if url == "" {
-		fmt.Printf("NYPL_API_URL not set, defaulting to %q\n", defaultUrl)
-		url = defaultUrl
-	}
-
-	client := nypl_exporter.NewClient(key, url)
-	if err := run(client); err != nil {
+	if err := run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func run(client *nypl_exporter.Client) error {
-	resp, err := client.ItemsTotal()
-	if err != nil {
-		return err
-	}
-	fmt.Println(resp.NYPLAPI.Response.Count.Value)
+func run() error {
 	return nil
 }
